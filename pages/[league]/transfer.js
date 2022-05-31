3c6cacd6e956a273e0eb8231619368d6e94a6f7b
Lukasdotcom <lukas@lschaefer.xyz>
2022-05-31 19:53:40 -0400

    Allows you to filter positions in the search

--- pages/[league]/transfer.js
+++ pages/[league]/transfer.js
@@ -4,11 +4,13 @@ import Head from "next/head"
 import { useState, useEffect } from "react"
 import Player from "../../components/Player"
 export default function Home({session, league, defaultSearch}) {
+    const positionList = ["gk", "def", "mid", "att"]
     const [players, setPlayers] = useState(defaultSearch)
     const [searchTerm, setSearchTerm] = useState("")
     const [finished, setFinished] = useState(false)
+    const [positions, setPositions] = useState(positionList)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => {search(true)}, [searchTerm])
+    useEffect(() => {search(true)}, [searchTerm, positions])
     // Used to search the isNew is used to check if it should reload everything back from the start
     async function search(isNew) {
         var length = -1
@@ -22,7 +24,7 @@ export default function Home({session, league, defaultSearch}) {
             setFinished(false)
         }
         // Gets the data and returns the amount of players found
-        const newLength = await fetch(`/api/player?${(isNew ? "" : `limit=${players.length + 10}` )}&searchTerm=${encodeURIComponent(searchTerm)}`).then(async (val) => {val = await val.json(); setPlayers(val); return val.length})
+        const newLength = await fetch(`/api/player?${(isNew ? "" : `limit=${players.length + 10}` )}&searchTerm=${encodeURIComponent(searchTerm)}&positions=${encodeURIComponent(JSON.stringify(positions))}`).then(async (val) => {val = await val.json(); setPlayers(val); return val.length})
         if (newLength == length) {
             setFinished(true)
         }
@@ -41,8 +43,16 @@ export default function Home({session, league, defaultSearch}) {
             search(false)
         }
     }}>
-    <label htmlFor="search">Enter Custom Invite Link Here: </label>
+    <label htmlFor="search">Search Player Name: </label>
     <input onChange={(val) => {setSearchTerm(val.target.value)}} val={searchTerm} id="search"></input>
+    <br></br>
+    { positionList.map((position) => 
+        // Used to select and unselect every position
+        <>
+        <label htmlFor={position}>{position}:</label>
+        <input type="checkbox" onChange={(e) => {e.target.checked ? setPositions([...positions, position]) : setPositions(positions.filter((e2) => e2 != position))}} checked={positions.includes(position)} id={position}></input>
+        </>
+    )}
     { players.map((val) => 
         <Player key={val} uid={val} />
     )}
