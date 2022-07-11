8577615d6063d004331b5fbd9db3ed9ca3a9deff
Lukasdotcom <lukas@lschaefer.xyz>
2022-07-11 11:04:30 -0400

    Adds a notice on the info screen on PWA

--- pages/info.js
+++ pages/info.js
@@ -19,6 +19,11 @@ export default function Home({session}) {
         All transfers are finalized an hour before the official manager&apos;s tranfers period ends to give you some time to setup your team. You are allowed 6 tranfers(a transfer is either a purchase or a sale) and can have unlimited players.
         You start with a budget of 150 million. You are only allowed to move a player onto thee field when the player has not played on that matchday.
     </p>
+    <h2>Installing as an App</h2>
+    <p>
+        I don&apos;t really want ot pay $100 to get an apple developer license so there is not an app you can install from the app store or play store. But if you want something close to an app you can install a PWA.
+        This will look like a regular app but won&apos;t cost me anything to host. To install this click <a style={{"color" : "blue"}} href="https://web.dev/learn/pwa/installation/" rel="noopener noreferrer" target="_blank">here for the steps on how to install a PWA</a>.
+    </p>
     </>
     )
 }
