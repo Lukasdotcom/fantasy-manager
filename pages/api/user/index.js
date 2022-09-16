cc3b2e101921f8c57b84d23c107237b95f576d25
Lukasdotcom <lukas@lschaefer.xyz>
2022-09-16 18:30:24 -0400

    Allows you to connect and disconnect from oauth providers

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -29,7 +29,32 @@ export default async function handler(req, res) {
           );
         console.log(`User ${id} changed password`);
         connection.end();
+      } else if (
+        req.body.provider === "google" ||
+        req.body.provider === "github"
+      ) {
+        // Used to update the username
+        const email = String(req.body.email);
+        const connection = await connect();
+        // Disconnects the email
+        if (email == "") {
+          connection.query(
+            `UPDATE users SET ${req.body.provider}='' WHERE id=?`,
+            [id]
+          );
+          console.log(`User ${id} disconnected from ${req.body.provider}`);
+          res.status(200).end(`Disconnected from ${req.body.provider}`);
+        } else {
+          connection.query(
+            `UPDATE users SET ${req.body.provider}=? WHERE id=?`,
+            [email, id]
+          );
+          console.log(`User ${id} connected to ${req.body.provider}`);
+          res.status(200).end(`Connected to ${req.body.provider}`);
+        }
+        connection.end();
       } else if (req.body.username === undefined) {
+        // Used to connect and disconnect from OAuth providers
         res.status(500).end("No username given");
       } else {
         const connection = await connect();
