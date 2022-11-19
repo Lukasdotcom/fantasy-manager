597d671026b737f0160d1b07e235062a9cfef521
Lukasdotcom <lukas@lschaefer.xyz>
2022-11-19 10:32:16 -0500

    Removes bundle analyzer

--- next.config.js
+++ next.config.js
@@ -2,23 +2,17 @@ const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.APP_ENV !== "production",
 });
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-  openAnalyzer: process.env.ANALYZE === "true",
-});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA(
-  withBundleAnalyzer({
-    reactStrictMode: true,
-    images: {
-      domains: [
-        "i.bundesliga.com",
-        "resources.premierleague.com",
-        "play.fifa.com",
-      ],
-    },
-  })
-);
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "i.bundesliga.com",
+      "resources.premierleague.com",
+      "play.fifa.com",
+    ],
+  },
+});
 
 module.exports = nextConfig;
