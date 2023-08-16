462c39d9d308f4a5db271774ebd89083d357545e
Lukasdotcom <lukas@lschaefer.xyz>
2023-08-16 10:50:23 -0400

    Allows download page to work when no leagues exist

--- pages/download.tsx
+++ pages/download.tsx
@@ -54,57 +54,62 @@ export default function Home({
         {t("Here you can download the player data for personal use. ")}
         {t("This downloaded player data is only available in english. ")}
       </p>
-      <InputLabel htmlFor="time">{t("Time")}</InputLabel>
-      <Select
-        id="time"
-        value={matchday}
-        onChange={(val) => setMatchday(val.target.value as number)}
-      >
-        {historicalTimes[league].map((e: number) => {
-          const date = new Date(e * 1000);
-          return (
-            <MenuItem key={e} value={e}>
-              {t("{date}", { date })}
-            </MenuItem>
-          );
-        })}
-        <MenuItem value={0}>{t("Latest")}</MenuItem>
-      </Select>
-      <InputLabel htmlFor="league">{t("League")}</InputLabel>
-      <Select value={league} onChange={changeLeague} id="league">
-        {leagues.map((val) => (
-          <MenuItem key={val} value={val}>
-            {t(val)}
-          </MenuItem>
-        ))}
-      </Select>
-      <br />
-      {!league_enabled[league] && (
-        <p>{t("Note that this league is not updated anymore.")}</p>
-      )}
-      <FormControlLabel
-        label={t("Download hidden players in addition to all other ones")}
-        control={
-          <Checkbox
-            id="showHidden"
-            checked={showHidden}
-            onChange={(e) => setShowHidden(e.target.checked)}
+      {leagues.length > 0 && (
+        <>
+          <InputLabel htmlFor="time">{t("Time")}</InputLabel>
+          <Select
+            id="time"
+            value={matchday}
+            onChange={(val) => setMatchday(val.target.value as number)}
+          >
+            {historicalTimes[league].map((e: number) => {
+              const date = new Date(e * 1000);
+              return (
+                <MenuItem key={e} value={e}>
+                  {t("{date}", { date })}
+                </MenuItem>
+              );
+            })}
+            <MenuItem value={0}>{t("Latest")}</MenuItem>
+          </Select>
+          <InputLabel htmlFor="league">{t("League")}</InputLabel>
+          <Select value={league} onChange={changeLeague} id="league">
+            {leagues.map((val) => (
+              <MenuItem key={val} value={val}>
+                {t(val)}
+              </MenuItem>
+            ))}
+          </Select>
+          <br />
+          {!league_enabled[league] && (
+            <p>{t("Note that this league is not updated anymore.")}</p>
+          )}
+          <FormControlLabel
+            label={t("Download hidden players in addition to all other ones")}
+            control={
+              <Checkbox
+                id="showHidden"
+                checked={showHidden}
+                onChange={(e) => setShowHidden(e.target.checked)}
+              />
+            }
           />
-        }
-      />
-      <br />
-      <ButtonGroup>
-        <Button>
-          <Link disableNext={true} href={downloadLink("json")}>
-            {t("Download as {file}", { file: "json" })}
-          </Link>
-        </Button>
-        <Button>
-          <Link disableNext={true} href={downloadLink("csv")}>
-            {t("Download as {file}", { file: "csv" })}
-          </Link>
-        </Button>
-      </ButtonGroup>
+          <br />
+          <ButtonGroup>
+            <Button>
+              <Link disableNext={true} href={downloadLink("json")}>
+                {t("Download as {file}", { file: "json" })}
+              </Link>
+            </Button>
+            <Button>
+              <Link disableNext={true} href={downloadLink("csv")}>
+                {t("Download as {file}", { file: "csv" })}
+              </Link>
+            </Button>
+          </ButtonGroup>
+        </>
+      )}
+      {leagues.length === 0 && <p>{t("No leagues exist.")}</p>}
     </>
   );
 }
