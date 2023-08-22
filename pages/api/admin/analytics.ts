9ea2f3737a7378c6dba6a1ad208702a567b8d775
Lukas <lukas@lschaefer.xyz>
2023-08-22 16:16:05 -0400

    Makes the analytics only load the needed data (#489)

---
+++ pages/api/admin/analytics.ts
@@ -0,0 +1,40 @@
+import connect from "#database";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "#/pages/api/auth/[...nextauth]";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  // Check if the user is logged in and an admin
+  const user = await getServerSession(req, res, authOptions);
+  if (!user) {
+    res.status(401).end("Not logged in");
+    return;
+  }
+  if (!user.user.admin) {
+    res.status(403).end("Not an admin");
+    return;
+  }
+  switch (req.method) {
+    case "GET":
+      // Gets analytics data
+      const connection = await connect();
+      const result = await connection.query(
+        "SELECT * FROM analytics WHERE day>=? AND day<?+50",
+        [req.query.day, req.query.day],
+      );
+      if (
+        process.env.NODE_ENV !== "development" &&
+        process.env.NODE_ENV !== "production"
+      ) {
+        res.setHeader("Cache-Control", `public, max-age=108000`);
+      }
+      res.status(200).json(result);
+      break;
+    default:
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+      break;
+  }
+}
