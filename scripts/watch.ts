7aae8d44ae4e64bdc10cca2cd97c3f49116b3bf6
Lukasdotcom <lukas@lschaefer.xyz>
2023-09-26 09:44:30 -0400

    Only revalidates all static pages on the very first boot in a docker image

--- scripts/watch.ts
+++ scripts/watch.ts
@@ -1,3 +1,5 @@
+import { existsSync, rmSync } from "fs";
+
 /**
  * Starts the watcher by calling the 'watcher' function at regular intervals.
  */
@@ -20,17 +22,22 @@ async function watcher() {
     try {
       await fetch(String(process.env.NEXTAUTH_URL_INTERNAL));
       server_booted = true;
-      // Revalidates all pages when the server boots
-      fetch(process.env.NEXTAUTH_URL_INTERNAL + "/api/revalidate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          path: "*",
-          secret: process.env.NEXTAUTH_SECRET,
-        }),
-      });
+      // Revalidates all pages when the server boots for the first time
+      if (existsSync("./revalidate")) {
+        console.log("Revalidating all pages due to first boot of docker image");
+        fetch(process.env.NEXTAUTH_URL_INTERNAL + "/api/revalidate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            path: "*",
+            secret: process.env.NEXTAUTH_SECRET,
+          }),
+        }).then(() => {
+          rmSync("./revalidate");
+        });
+      }
     } catch (_) {
       // If the server takes more than 10 minutes to boot then exit
       if (Date.now() - startup > 600000) {
