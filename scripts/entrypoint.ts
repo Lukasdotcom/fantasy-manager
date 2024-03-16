759f62850b127a3ced04b3a950be21434f527376
Lukasdotcom <lukas@lschaefer.xyz>
2024-03-15 22:13:37 -0400

    Corrects bad test data

--- scripts/entrypoint.ts
+++ scripts/entrypoint.ts
@@ -19,7 +19,9 @@ if (process.env.APP_ENV !== "test") {
 startWatcher();
 checkPictures();
 // Makes sure to check if an action is neccessary every 10 seconds
-setInterval(update, 10000);
+if (process.env.APP_ENV !== "test") {
+  setInterval(update, 10000);
+}
 /**
  * Updates all leagues.
  */
